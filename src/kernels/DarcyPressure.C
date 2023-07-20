3fb1adb37493042e283f9aff73fd8d9ac789fe05
lscotti <luciascotti.ls@gmail.com>
2023-07-20 10:24:59 +0100

    defined "permeability" and "viscosity" input parameters

---
+++ src/kernels/DarcyPressure.C
@@ -0,0 +1,28 @@
+#include "DarcyPressure.h"
+
+registerMooseObject("BabblerApp", DarcyPressure);
+InputParameters
+DarcyPressure::validParams()
+{
+	InputParameters params = ADKernelGrad::validParams();
+	params.addClassDescription("compute the diffusion term for Darcy pressure ($p$) equation: "
+			          "$-\\nabla \\cdot \\frac{\\mathbf{K}}{\\mu} \\nabla p =0$");
+	params.addRequiredParam<Real>("permeability", "The isotropic permeability ($K) of the medium");
+        params.addParam<Real>(
+			"viscosity",
+			7.98e-04,
+			"The dynamic viscosity ($\\mu$) of the fluid, the default is the water at 30 "
+			"degrees Celsius (7.98-04 Pa-s)");	
+	return params;
+}
+DarcyPressure::DarcyPressure(const InputParameters & parameters)
+	: ADKernelGrad(parameters),
+	_permeability(getParam<Real>("permeability")),
+	_viscosity(getParam<Real>("viscosity"))
+{
+}
+ADRealVectorValue
+DarcyPressure::precomputeQpResidual()
+{
+	return (_permeability/_viscosity)*_grad_u[_qp];
+}
